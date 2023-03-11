e71a29b8fc8058535473ae68c945798d8c696bfd
Polinica <liusikova.vika@yandex.ru>
2023-03-11 14:03:59 +0300

    6 elements

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,53 @@
+/** Исходные карточки для загрузки */
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const cardsContainer = document.querySelector(".elements");
+const cardTemplate = document.querySelector("#element").content;
+
+function renderCards(container, ...elements) {
+  elements.forEach((element) => {
+    container.prepend(getNewCard(element.name, element.link));
+  });
+}
+
+/** Функция создает из шаблона элемент с новой карточкой и возвращает его */
+function getNewCard(name, link) {
+  // Создание элемента из шаблона
+  const card = cardTemplate.querySelector(".element").cloneNode(true);
+
+  // Заполнение содержимого
+  card.querySelector(".element__image").src = link;
+  card.querySelector(".element__image").alt = name;
+  card.querySelector(".element__title").textContent = name;
+
+  return card;
+}
+
 // POPUP
 let profileEdit = document.querySelector(".profile__edit-btn"),
   profileName = document.querySelector(".profile__name-title"),
@@ -31,3 +81,6 @@ function handleFormSubmit(evt) {
 form.addEventListener("submit", handleFormSubmit);
 profileEdit.addEventListener("click", openToClick);
 closeInput.addEventListener("click", closeToClick);
+
+/** Отобразить исходные карточки при загрузке страницы */
+renderCards(cardsContainer, ...initialCards);
