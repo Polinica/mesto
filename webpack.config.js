c2ac4600a8cc45ad535b22af493de78691e7734d
Polinica <liusikova.vika@yandex.ru>
2023-05-01 22:53:23 +0300

    develope/ установка и настройка PostCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path"); // подключаем path к конфигу вебпак
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // подключите плагин
 const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // подключили плагин
+// подключите к проекту mini-css-extract-plugin
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: { main: "./src/index.js" },
@@ -29,6 +31,29 @@ module.exports = {
         // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: "/node_modules/",
       },
+      // добавили правило для обработки файлов
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      // добавьте ещё одно правило:
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            // добавьте объект options
+            options: { importLoaders: 1 },
+          },
+          // Добавьте postcss-loader
+          "postcss-loader",
+        ],
+      },
     ],
   },
   plugins: [
@@ -36,5 +61,6 @@ module.exports = {
       template: "./src/index.html", // путь к файлу index.html
     }),
     new CleanWebpackPlugin(), // использовали плагин
+    new MiniCssExtractPlugin(), // подключение плагина для объединения файлов
   ], // добавьте массив
 };
