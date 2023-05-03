a7d0657332ea70c3b2a8296d87fc0efdc4d9643c
Polinica <liusikova.vika@yandex.ru>
2023-05-03 23:55:20 +0300

    refact/Card.js - likeCard

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -12,12 +12,14 @@ export default class PopupWithForm extends Popup {
     super(popupSelector);
     this._handleSubmit = handleSubmit;
     this._form = this._popup.querySelector(".popup__form");
-    this._inputValues = {};
-    this._inputList = this._form.querySelectorAll('.popup__input');
+    //this._inputValues = {};
+
+    this._inputList = this._form.querySelectorAll(".popup__input");
   }
 
   _getInputValues() {
-    this._form.querySelectorAll(".popup__input").forEach((input) => {
+    this._inputValues = {};
+    this._inputList.forEach((input) => {
       this._inputValues[input.name] = input.value;
     });
     return this._inputValues;
